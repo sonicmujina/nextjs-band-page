6d883de7e2e43866838d51a9281d1406c6937801
James <z5311868@ad.unsw.edu.au>
2024-06-06 17:57:53 +1000

    fixed errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import fishpoxGroup from "/public/assets/FishpoxGroupPhoto.jpeg";
+import fishpoxGroup from "/public/assets/FishPoxGroupPhoto.jpeg";
 import fishing from "/public/assets/fishingClose.png";
 
 const AboutPage = () => {
